baba9540ddc45865d586420540b2816f01af2be2
Berkay Kazkılınç <75437423+berkaykazkilinc@users.noreply.github.com>
2022-04-07 00:36:02 +0300

    Create Odev_4.sql

---
+++ Odev_4.sql
@@ -0,0 +1,29 @@
+--https://app.patika.dev/dirittidarida
+/*
+--1.SORU
+SELECT DISTINCT replacement_cost
+FROM film
+*/
+/*
+--2.SORU
+SELECT COUNT(DISTINCT replacement_cost)
+FROM film
+*/
+/*
+--3.SORU
+SELECT COUNT(*)
+FROM film
+WHERE title LIKE 'T%' AND rating = 'G'
+*/
+/*
+--4.SORU
+SELECT COUNT(*)
+FROM country
+WHERE country LIKE '_____'
+*/
+/*
+--5.SORU
+SELECT COUNT(*)
+FROM city
+WHERE city LIKE '%R' OR city LIKE '%r'
+*/
