1a7d4591320c4671e03d257b65f4373e1215ac15
Berkay Kazkılınç <75437423+berkaykazkilinc@users.noreply.github.com>
2022-04-07 01:33:36 +0300

    Create Odev_9.sql

---
+++ Odev_9.sql
@@ -0,0 +1,20 @@
+--https://app.patika.dev/dirittidarida
+/*
+--1.SORU 
+SELECT city,country
+FROM city
+JOIN country ON city.country_id = country.country_id
+*/
+/*
+--2.SORU
+SELECT payment_id,first_name,last_name 
+FROM customer
+INNER JOIN payment ON customer.customer_id = payment.customer_id
+*/
+
+/*
+--3.SORU
+SELECT rental_id,first_name,last_name 
+FROM customer
+INNER JOIN rental ON customer.customer_id = rental.customer_id
+*/
