4f2edb20486161f18702fc8281d72bdb5f24cc63
Berkay Kazkılınç <75437423+berkaykazkilinc@users.noreply.github.com>
2022-04-07 00:26:22 +0300

    Create Odev_3.sql

---
+++ Odev_3.sql
@@ -0,0 +1,25 @@
+/*
+--1.SORU
+SELECT country
+FROM country
+WHERE country LIKE  'A%a'
+*/
+/*
+--2.SORU
+SELECT country 
+FROM country
+WHERE country LIKE '_____%n'
+*/
+/*
+--3.SORU
+SELECT title FROM film
+WHERE title ILIKE '%t%t%t%t%'
+*/
+/*
+--4.SORU
+SELECT * FROM film
+WHERE title ILIKE 'C%' AND length > 90 AND rental_rate = 2.99
+*/
+
+
+--https://app.patika.dev/dirittidarida
